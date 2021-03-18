40eb838cf973a6e2855a55ba9dc3396209572c79
Ismail Bouhiri <ibouhiri@e2r9p8.1337.ma>
2021-03-18 12:50:26 +0100

    .

---
+++ Module05/ex00/Bureaucrat.cpp
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibouhiri <ibouhiri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/18 11:44:34 by ibouhiri          #+#    #+#             */
+/*   Updated: 2021/03/18 12:50:16 by ibouhiri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat( std::string name, int grade ) : _Name(name)
+{
+	try
+	{
+		if (grade > 150)
+		{
+			throw GradeTooLowException();
+		}
+		else if (grade < 1)
+		{
+			throw GradeTooHighException();
+		}
+		else{
+			this->_Grade = grade;
+		}
+	}
+	catch(Bureaucrat::GradeTooLowException& e)
+	{
+		std::cout << e.lower() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooHighException& e)
+	{
+		std::cout << e.higher() << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
+
+Bureaucrat::~Bureaucrat( void )
+{}
+
+std::string const Bureaucrat::getName( void ) const
+{
+	return this->_Name;
+}
+
+int	Bureaucrat::getGrade( void ) const
+{
+	return this->_Grade;
+}
+
+void	Bureaucrat::incrementGrade( void )
+{
+	try
+	{
+		this->_Grade -= 1;
+		if (this->_Grade < 1)
+			throw GradeTooHighException();
+	}
+	catch(Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << e.higher() << std::endl;
+	}
+}
+
+void	Bureaucrat::decrementGrade( void )
+{
+	try
+	{
+		this->_Grade += 1;
+		if (this->_Grade > 150)
+			throw GradeTooLowException();
+	}
+	catch(Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << e.lower() << std::endl;
+	}
+	
+}
