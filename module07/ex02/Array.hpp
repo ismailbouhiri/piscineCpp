282c85c163a0cb5273177c799dae0bae21862012
Ismail Bouhiri <ibouhiri@e2r9p8.1337.ma>
2021-03-28 18:53:22 +0100

    .

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,7 +6,24 @@
 /*   By: ibouhiri <ibouhiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 16:35:11 by ibouhiri          #+#    #+#             */
-/*   Updated: 2021/03/27 16:35:12 by ibouhiri         ###   ########.fr       */
+/*   Updated: 2021/03/28 18:53:08 by ibouhiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+# include <iostream>
+# include <string>
+
+// template< typename T>
+// class Array<T>
+// {
+// private:
+// 	/* data */
+// public:
+// 	Array<T>( void );
+// 	~Array<T> ( void );
+// };
+
+#endif
