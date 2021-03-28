34affd2c667d5899905d37f0aa14a0fdaa53d5c8
Ismail Bouhiri <ibouhiri@e2r9p8.1337.ma>
2021-03-28 18:17:55 +0100

    .

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -6,7 +6,31 @@
 /*   By: ibouhiri <ibouhiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 16:34:55 by ibouhiri          #+#    #+#             */
-/*   Updated: 2021/03/27 16:34:56 by ibouhiri         ###   ########.fr       */
+/*   Updated: 2021/03/28 18:17:40 by ibouhiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "whatever.hpp"
+
+int		main( void )
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return ( 0 );
+}
--- module07/ex00/whatever.hpp
+++ module07/ex00/whatever.hpp
@@ -6,7 +6,37 @@
 /*   By: ibouhiri <ibouhiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 16:34:59 by ibouhiri          #+#    #+#             */
-/*   Updated: 2021/03/27 16:35:00 by ibouhiri         ###   ########.fr       */
+/*   Updated: 2021/03/28 18:14:50 by ibouhiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+# include <iostream>
+# include <string>
+
+template <typename T>
+void swap( T& a,  T& b)
+{
+	T c;
+
+	c = a;
+	a = b;
+	b = c;
+}
+
+template <typename T>
+const T& min(const T& a, const T& b)
+{
+	return ( (a <= b ) ? a : b );
+}
+
+template <typename T>
+const T& max(const T& a, const T& b)
+{
+	return ( (a >= b ) ? a : b );
+}
+
+#endif
 
