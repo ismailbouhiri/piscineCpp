a62df215db7ebe9838f354e4aa38031ad4dadad7
Ismail Bouhiri <ibouhiri@e2r9p8.1337.ma>
2021-03-28 18:42:21 +0100

    .

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -6,7 +6,24 @@
 /*   By: ibouhiri <ibouhiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 16:35:02 by ibouhiri          #+#    #+#             */
-/*   Updated: 2021/03/27 16:35:03 by ibouhiri         ###   ########.fr       */
+/*   Updated: 2021/03/28 18:38:28 by ibouhiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+# include <string>
+# include <iostream>
+
+template <typename U>
+void	iter( const U *array, const int& len, void (*f)(U const& ))
+{
+	for (int i = 0; i < len; i++)
+	{
+		f(array[i]);
+	}
+}
+
+#endif
 
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,7 +6,23 @@
 /*   By: ibouhiri <ibouhiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 16:35:04 by ibouhiri          #+#    #+#             */
-/*   Updated: 2021/03/27 16:35:05 by ibouhiri         ###   ########.fr       */
+/*   Updated: 2021/03/28 18:41:57 by ibouhiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "iter.hpp"
+
+template<typename T>
+void print(const T& arg)
+{
+    std::cout<< "the number is " << arg << std::endl;
+}
+
+int		main( void )
+{
+    int iarr[3] = {150, 16, 70};
+    float farr[5] = {60.0f, 50.8f, 150.16f, 2.0f, 100.1f};
+
+    iter(iarr, 3, print);
+    iter(farr, 5, print);
+}
