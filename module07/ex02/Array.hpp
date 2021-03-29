7af103c4ea5915059c45c7e276b78088f319d1b9
Ismail Bouhiri <ibouhiri@e2r9p8.1337.ma>
2021-03-29 10:15:20 +0100

    .

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ibouhiri <ibouhiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 16:35:11 by ibouhiri          #+#    #+#             */
-/*   Updated: 2021/03/28 18:53:08 by ibouhiri         ###   ########.fr       */
+/*   Updated: 2021/03/29 10:12:03 by ibouhiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,72 @@
 # include <iostream>
 # include <string>
 
-// template< typename T>
-// class Array<T>
-// {
-// private:
-// 	/* data */
-// public:
-// 	Array<T>( void );
-// 	~Array<T> ( void );
-// };
+template<typename T=int>
+class Array
+{
+
+private:
+
+	unsigned int _N;
+	T *_Array;
+
+public:
+
+	Array<T>( void ): _N(0){
+		this->_Array(nullptr);
+	}
+
+	Array<T>( const unsigned int& n ) : _N(n) {
+		this->_Array = new T[n]();
+		if ( !this->_Array )
+			std::cout << "allocation failled !!" << *_Array << std::endl;
+	}
+
+	Array<T>( const Array<T>& CObj ) {
+		*this = CObj;
+	}
+	
+	Array& operator=( const Array<T>& CObj ) {
+
+		if (this->_Array[0]) {
+			delete [] this->_Array;
+		}
+		else {
+
+			this->_N = CObj.size();
+			this->_Array = new T[this->_N]();
+			for (unsigned int i = 0; i < this->_N; i++)
+			{
+				this->_Array[i] = CObj._Array[i];
+			}
+		}
+		return ( *this );
+	}
+
+	T& operator[]( const unsigned int& i )
+	{
+		if (i < this->_N ) {
+			return this->_Array[i];
+		}
+		else {
+			throw std::exception();
+		}
+	}
+
+	~Array<T> ( void ){
+		if (_Array[0]) {
+			delete [] this->_Array;
+		}
+	};
+
+	const unsigned int& size( void ) const {
+		return this->_N;
+	}
+
+	T& getArray( void ) const {
+		return this->_Array;
+	}
+	
+};
 
 #endif
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -6,7 +6,48 @@
 /*   By: ibouhiri <ibouhiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 16:35:13 by ibouhiri          #+#    #+#             */
-/*   Updated: 2021/03/27 16:35:14 by ibouhiri         ###   ########.fr       */
+/*   Updated: 2021/03/29 09:47:31 by ibouhiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "Array.hpp"
+
+int		main( void )
+{
+    Array<int> arr(6);
+    Array<int> arr1(5);
+    Array<char> char_arr(20);
+
+    try
+    {
+        arr[0] = 4;
+        std::cout << arr[0] << std::endl;
+        arr = arr1;
+        arr1[0] = 5;
+        arr[0] = 6;
+        char_arr[0] = 'a';
+        char_arr[1] = 'b';
+
+        std::cout << arr[0] << " "<< arr1[0] << std::endl;
+
+        for ( size_t i = 0; i < arr.size(); i++ )
+            std::cout << arr[i] << " ";
+        std::cout << std::endl;
+
+        for ( size_t i = 0; i < arr1.size(); i++ )
+            std::cout << arr1[i] << " ";
+        std::cout << std::endl;
+
+        for (size_t i = 0; i < char_arr.size(); i++)
+            std::cout << char_arr[i] << " ";
+        std::cout << std::endl;
+
+        arr1[5] = 10;
+        std::cout << arr[2] << std::endl;
+    }
+    catch( const std::exception& e )
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return ( 0 );
+}
